9c78b5f14044b581cd6662eda6a259af218f5843
Pascal Jungblut <mail@pascalj.de>
2025-10-16 23:13:33 +0200

    Switch to buildRustPackage to have a proper build
    crane does not build because of relative paths in the
    proto includes.
    crate2nix does not build because of the non-flat structure of
    the axum crate.

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    naersk.url = "github:nix-community/naersk/master";
+    crane.url = "github:ipetkov/crane";
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     utils.url = "github:numtide/flake-utils";
   };
@@ -10,30 +10,55 @@
       self,
       nixpkgs,
       utils,
-      naersk,
+      crane,
     }:
     utils.lib.eachDefaultSystem (
       system:
       let
         pkgs = import nixpkgs { inherit system; };
-        naersk-lib = pkgs.callPackage naersk { };
+        craneLib = crane.mkLib pkgs;
       in
       {
-        defaultPackage = naersk-lib.buildPackage ./.;
-        devShell =
-          with pkgs;
-          mkShell {
-            buildInputs = [
-              cargo
-              runc
-              rustc
-              rustfmt
-              pre-commit
-              rustPackages.clippy
-              protobuf
-            ];
-            RUST_SRC_PATH = rustPlatform.rustLibSrc;
+        defaultPackage = pkgs.rustPlatform.buildRustPackage {
+
+          pname = "snarf";
+          version = "0.0.1";
+
+          src = pkgs.lib.cleanSource ./.;
+
+          cargoLock = {
+            lockFile = ./Cargo.lock;
+            outputHashes = {
+              "nar-bridge-0.1.0" = "sha256-a/WYaBl5xluFUyvOh/hPgR0bjBcL7GfS0fpFxIV49cc=";
+              "wu-manber-0.1.0" = "sha256-7YIttaQLfFC/32utojh2DyOHVsZiw8ul/z0lvOhAE/4=";
+            };
           };
+
+          meta = with pkgs.lib; {
+            description = "A fast line-oriented regex search tool, similar to ag and ack";
+            homepage = "https://github.com/BurntSushi/ripgrep";
+            license = licenses.unlicense;
+            maintainers = [ ];
+          };
+
+          # RUST_SRC_PATH = pkgs.rustPlatform.rustLibSrc;
+          # pkgs.rustPlatform.rustLibSrc
+          buildInputs = with pkgs; [
+            cargo
+            runc
+            rustc
+            rustfmt
+            pre-commit
+            rustPackages.clippy
+            protobuf
+          ];
+
+          nativeBuildInputs = with pkgs; [
+            protobuf
+          ];
+        };
+
+        devShells.defaulf = craneLib.devShell { };
       }
     );
 }
