b9541348470593a8434a1fc4543ad5ab3b6d71ce
Pascal Jungblut <mail@pascalj.de>
2025-11-15 18:51:51 +0100

    Use signals to shutdown/reload server

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,12 +1,14 @@
 use std::{
     path::{Path, PathBuf},
-    sync::{Arc, RwLock},
+    sync::Arc,
 };
 
 use clap::Parser;
 
 use ed25519_dalek::{pkcs8::DecodePrivateKey, pkcs8::EncodePrivateKey};
-use snarf::server::{self, CacheKeypair};
+use snarf::server::{self, CacheKeypair, ServerCommand};
+
+use tokio::sync::mpsc;
 
 use tracing::info;
 
@@ -77,9 +79,6 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
 
     let arguments = Arguments::parse();
 
-    let (blob_service, directory_service, path_info_service, nar_calculation_service) =
-        snix_store::utils::construct_services(arguments.service_addrs).await?;
-
     let paseto_key = if arguments.paseto_key_file.exists() {
         load_paseto_keypair(&arguments.paseto_key_file)
     } else {
@@ -91,66 +90,100 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     } else {
         serialize_new_cache_keypair(arguments.cache_keypair_file)
     }?;
+    let new_server_state = Arc::new(std::sync::Mutex::new(server::ServerState::new(
+        &paseto_key,
+        &cache_key,
+    )));
 
-    let server_state = server::ServerState::new(&paseto_key, &cache_key);
+    loop {
+        let (command_sender, mut command_receiver) = mpsc::channel::<ServerCommand>(8);
+        let do_shutdown = Arc::new(std::sync::Mutex::new(false));
 
-    // The signing_path_info service will sign only while serving new path_infos.
-    let signing_path_info_service = Arc::new(server::LazySigningPathInfoService::new(
-        path_info_service.clone(),
-        cache_key.clone(),
-    ));
+        let do_shutdown_copy = do_shutdown.clone();
+        let new_server_state_copy = new_server_state.clone();
+        let shutdown = async move {
+            info!("Press Cltr-C for graceful shutdown.");
+            tokio::select! {
+                _= tokio::signal::ctrl_c() => {
+                    *do_shutdown_copy.lock().unwrap() = true;
+                }
+                action = command_receiver.recv() => {
+                    match action {
+                        Some(ServerCommand::UpdateState(state)) => { *new_server_state_copy.lock().unwrap() = state },
+                        Some(ServerCommand::Shutdown) => {
+                            *do_shutdown_copy.lock().unwrap() = false;
+                        },
+                        None => {}
+                    }
+                }
+            }
+        };
 
-    // The management channels are used to fill the cache and potentially to configure
-    // it, authenticated.
-    let management_routes = snarf::server::server_routes(
-        Arc::new(RwLock::new(server_state)),
-        blob_service.clone(),
-        directory_service.clone(),
-        signing_path_info_service.clone(),
-        nar_calculation_service,
-    );
+        let (blob_service, directory_service, path_info_service, nar_calculation_service) =
+            snix_store::utils::construct_services(arguments.service_addrs.clone()).await?;
+        // The signing_path_info service will sign only while serving new path_infos.
+        let signing_path_info_service = Arc::new(server::LazySigningPathInfoService::new(
+            path_info_service.clone(),
+            cache_key.clone(),
+        ));
 
-    // The nar-bridge serves the actual cache data, unauthenticated.
-    let nar_bridge_state = nar_bridge::AppState::new(
-        blob_service.clone(),
-        directory_service.clone(),
-        signing_path_info_service,
-        std::num::NonZero::new(64usize).unwrap(),
-    );
+        let server_state = new_server_state.clone().lock().unwrap().clone();
 
-    // HTTP
-    let app = nar_bridge::gen_router(30)
-        .with_state(nar_bridge_state)
-        .merge(management_routes.into_axum_router());
+        // The management channels are used to fill the cache and potentially to configure
+        // it, authenticated.
+        let management_routes = snarf::server::server_routes(
+            &command_sender,
+            &server_state,
+            blob_service.clone(),
+            directory_service.clone(),
+            signing_path_info_service.clone(),
+            nar_calculation_service,
+        );
 
-    let listen_address = &arguments.listen_args.listen_address.unwrap_or_else(|| {
-        "[::]:9000"
-            .parse()
-            .expect("invalid fallback listen address")
-    });
+        // The nar-bridge serves the actual cache data, unauthenticated.
+        let nar_bridge_state = nar_bridge::AppState::new(
+            blob_service.clone(),
+            directory_service.clone(),
+            signing_path_info_service,
+            std::num::NonZero::new(64usize).unwrap(),
+        );
 
-    let listener = tokio_listener::Listener::bind(
-        listen_address,
-        &Default::default(),
-        &arguments.listen_args.listener_options,
-    )
-    .await;
+        // HTTP
+        let app = nar_bridge::gen_router(30)
+            .with_state(nar_bridge_state)
+            .merge(management_routes.into_axum_router());
 
-    info!(listen_address=%listen_address, "starting daemon");
+        let listen_address = &arguments
+            .listen_args
+            .listen_address
+            .clone()
+            .unwrap_or_else(|| {
+                "[::]:9000"
+                    .parse()
+                    .expect("invalid fallback listen address")
+            });
 
-    let shutdown = async {
-        info!("Press Cltr-C for graceful shutdown.");
-        tokio::signal::ctrl_c()
-            .await
-            .expect("failed to install Ctrl-C handler");
-    };
+        let listener = tokio_listener::Listener::bind(
+            listen_address,
+            &Default::default(),
+            &arguments.listen_args.listener_options,
+        )
+        .await;
 
-    tokio_listener::axum07::serve(
-        listener.unwrap(),
-        app.into_make_service_with_connect_info::<tokio_listener::SomeSocketAddrClonable>(),
-    )
-    .with_graceful_shutdown(shutdown)
-    .await?;
+        info!(listen_address=%listen_address, "starting daemon");
+
+        let serve = tokio_listener::axum07::serve(
+            listener.unwrap(),
+            app.into_make_service_with_connect_info::<tokio_listener::SomeSocketAddrClonable>(),
+        )
+        .with_graceful_shutdown(shutdown);
+
+        serve.await?;
+
+        if *do_shutdown.lock().unwrap() {
+            break;
+        }
+    }
 
     Ok(())
 }
--- src/server.rs
+++ src/server.rs
@@ -11,6 +11,7 @@ use rusty_paseto::prelude::*;
 use snix_castore::{blobservice::BlobService, directoryservice::DirectoryService};
 use snix_store::{nar::NarCalculationService, pathinfoservice::PathInfoService};
 
+use tokio::sync::mpsc;
 use tonic::{async_trait, service::Interceptor};
 
 use tracing::{error, info};
@@ -68,7 +69,12 @@ pub enum Error {
     MissingSeparator,
 }
 
-#[derive(Clone, Eq, PartialEq)]
+pub enum ServerCommand {
+    UpdateState(ServerState),
+    Shutdown,
+}
+
+#[derive(Clone, Eq, PartialEq, Debug)]
 enum ServerInitialization {
     /// The server is not initialized and does not have a secret key.
     Uninitialized(PathBuf),
@@ -79,8 +85,10 @@ enum ServerInitialization {
 }
 
 /// State that is needed to perform operations on the PASETO tokens.
+#[derive(Clone)]
 pub struct ServerState {
     initialization: ServerInitialization,
+
     /// The underlying signing key bytes. First 32 bytes are private key, remaining 32 bytes are the public key.
     /// We use ed25519-dalek SigningKey here and just dynamically create the PasetoAsymmetric keys.
     paseto_keypair: PasetoKeypair,
@@ -162,52 +170,13 @@ impl ServerState {
     }
 }
 
-impl TryFrom<&[u8]> for ServerState {
-    type Error = &'static str;
-
-    fn try_from(bytes: &[u8]) -> Result<Self, Self::Error> {
-        if let Ok(bytes_arr) = bytes.try_into() {
-            return Ok(Self {
-                initialization: ServerInitialization::Initialized,
-                paseto_keypair: PasetoKeypair::from_keypair_bytes(bytes_arr)
-                    .map_err(|_| "Failed to generate SigningKey from bytes")?,
-                // TODO: fix it to use its own bytes
-                cache_keypair: CacheKeypair::new(
-                    "snarf".into(),
-                    Some(
-                        ed25519_dalek::SigningKey::from_keypair_bytes(bytes_arr)
-                            .map_err(|_| "Failed to generate SigningKey from bytes")?,
-                    ),
-                ),
-            });
-        }
-
-        Err("Failed to contruct PasetoState from bytes")
-    }
-}
-
-impl ServerState {
-    pub fn uninitialized<P: AsRef<std::path::Path>>(out_key_path: P) -> Self {
-        Self {
-            initialization: ServerInitialization::Uninitialized(
-                out_key_path.as_ref().to_path_buf(),
-            ),
-            paseto_keypair: ed25519_dalek::SigningKey::generate(&mut OsRng),
-            cache_keypair: CacheKeypair::new(
-                "snarf".into(),
-                Some(ed25519_dalek::SigningKey::generate(&mut OsRng)),
-            ),
-        }
-    }
-}
-
 #[derive(Clone)]
 /// An interceptor for the gRPC endpoint. It validates the incoming token
 /// on the server and ensures that the client has the sufficient permissions to
 /// perform a certain action.
 struct PasetoAuthInterceptor {
     /// The server state to use for authentication.
-    state: Arc<RwLock<ServerState>>,
+    state: ServerState,
 }
 
 impl Interceptor for PasetoAuthInterceptor {
@@ -231,12 +200,7 @@ impl Interceptor for PasetoAuthInterceptor {
             .ok_or_else(|| tonic::Status::unauthenticated("invalid authentication scheme"))?;
 
         // TODO: split this off into capabilities
-        if !self
-            .state
-            .read()
-            .map_err(|_| tonic::Status::internal("unable to get lock"))?
-            .verify_token(token)
-        {
+        if !self.state.verify_token(token) {
             return Err(tonic::Status::unauthenticated(
                 "invalid authentication scheme",
             ));
@@ -246,9 +210,11 @@ impl Interceptor for PasetoAuthInterceptor {
     }
 }
 
-impl From<Arc<RwLock<ServerState>>> for PasetoAuthInterceptor {
-    fn from(state: Arc<RwLock<ServerState>>) -> Self {
-        Self { state }
+impl From<&ServerState> for PasetoAuthInterceptor {
+    fn from(state: &ServerState) -> Self {
+        Self {
+            state: state.clone(),
+        }
     }
 }
 
@@ -327,7 +293,8 @@ where
 /// Get the routes used for the server. These will route the usual services but additionally
 /// provide a check for authentication.
 pub fn server_routes(
-    server_state: Arc<RwLock<ServerState>>,
+    command_channel: &mpsc::Sender<ServerCommand>,
+    server_state: &ServerState,
     blob_service: Arc<dyn BlobService>,
     directory_service: Arc<dyn DirectoryService>,
     path_info_service: Arc<dyn PathInfoService>,
@@ -336,13 +303,13 @@ pub fn server_routes(
     tonic::service::Routes::new(
         snix_castore::proto::blob_service_server::BlobServiceServer::with_interceptor(
             snix_castore::proto::GRPCBlobServiceWrapper::new(blob_service),
-            PasetoAuthInterceptor::from(server_state.clone()),
+            PasetoAuthInterceptor::from(server_state),
         ),
     )
     .add_service(
         snix_castore::proto::directory_service_server::DirectoryServiceServer::with_interceptor(
             snix_castore::proto::GRPCDirectoryServiceWrapper::new(directory_service),
-            PasetoAuthInterceptor::from(server_state.clone()),
+            PasetoAuthInterceptor::from(server_state),
         ),
     )
     .add_service(
@@ -351,22 +318,26 @@ pub fn server_routes(
                 path_info_service,
                 nar_calculation_service,
             ),
-            PasetoAuthInterceptor::from(server_state.clone()),
+            PasetoAuthInterceptor::from(server_state),
         ),
     )
     .add_service(management_service_server::ManagementServiceServer::new(
-        ManagementServiceServer::from(server_state.clone()),
+        ManagementServiceServer::new(command_channel, server_state),
     ))
 }
 
 #[derive(Clone)]
 pub struct ManagementServiceServer {
-    server_state: Arc<RwLock<ServerState>>,
+    server_state: ServerState,
+    command_channel: mpsc::Sender<ServerCommand>,
 }
 
-impl From<Arc<RwLock<ServerState>>> for ManagementServiceServer {
-    fn from(server_state: Arc<RwLock<ServerState>>) -> Self {
-        Self { server_state }
+impl ManagementServiceServer {
+    fn new(command_channel: &mpsc::Sender<ServerCommand>, server_state: &ServerState) -> Self {
+        Self {
+            command_channel: command_channel.clone(),
+            server_state: server_state.clone(),
+        }
     }
 }
 
@@ -376,18 +347,21 @@ impl management_service_server::ManagementService for ManagementServiceServer {
         &self,
         _: tonic::Request<NewClientTokenRequest>,
     ) -> Result<tonic::Response<ClientToken>, tonic::Status> {
-        let mut state = self
-            .server_state
-            .write()
-            .map_err(|_| tonic::Status::internal("Unable to acquire server lock"))?;
+        let mut state = self.server_state.clone();
 
         match state.initialization {
             ServerInitialization::Uninitialized(_) => {
                 info!("Generating new admin token");
                 state.initialize_signing_key();
+                let token = state.public_token();
+
+                self.command_channel
+                    .send(ServerCommand::UpdateState(state))
+                    .await
+                    .unwrap();
 
                 Ok(tonic::Response::new(ClientToken {
-                    token: state.public_token().map_err(|_| {
+                    token: token.map_err(|_| {
                         tonic::Status::internal("Unable to generate token from state")
                     })?,
                 }))
@@ -397,38 +371,6 @@ impl management_service_server::ManagementService for ManagementServiceServer {
                 "Server is already initialized",
             )),
         }
-    }
-}
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn successful_token() {
-        let server_state = ServerState::uninitialized(&PathBuf::from("/dummy"));
-        assert!(server_state.verify_token(server_state.public_token().unwrap().as_ref()))
-    }
-
-    #[test]
-    fn invalid_token() {
-        let server_state = ServerState::uninitialized(&PathBuf::from("/dummy"));
-        let different_server_state = ServerState::uninitialized(&PathBuf::from("/dummy"));
-        assert!(!server_state.verify_token(different_server_state.public_token().unwrap().as_ref()))
-    }
-
-    #[test]
-    fn server_state_from_bytes() {
-        let server_state = ServerState::uninitialized(&PathBuf::from("/dummy"));
-        assert!(ServerState::try_from(server_state.key_bytes().as_slice()).is_ok())
-    }
-
-    #[test]
-    fn server_state_from_invalid_bytes() {
-        let server_state = ServerState::uninitialized(&PathBuf::from("/dummy"));
-        let mut bytes = server_state.key_bytes();
-        // change a random bytes
-        bytes[4] += 1;
-        assert!(!ServerState::try_from(bytes.as_slice().as_ref()).is_ok())
     }
 }
 
