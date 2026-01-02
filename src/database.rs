a59af04acb08b9e65618e664a2c70721db9f4c64
Pascal Jungblut <mail@pascalj.de>
2026-01-02 18:01:37 +0100

    Add `client add <store-path>` command
    This gets rid of the implicit dependency on a Nix binary. However, it
    still requires access to Nix' sqlite database to construct the closure.

--- src/database.rs
+++ src/database.rs
@@ -1,23 +1,124 @@
+use std::collections::HashMap;
+
+use nix_compat::{
+    narinfo::Signature,
+    nixhash::{HashAlgo, NixHash},
+    store_path::StorePathRef,
+};
 use rusqlite::{Connection, Result};
 
-pub fn closure() -> Result<()> {
-    let conn = Connection::open("/nix/var/nix/db/db.sqlite")?;
+/// Representation of a closure computed from the database.
+///
+/// The closure is computed once for a path and it is only valid
+/// to query for information related to that path.
+pub struct Closure {
+    path_infos: Vec<LocalPathInfo>,
+    references: HashMap<u64, Vec<nix_compat::store_path::StorePath<String>>>,
+}
 
+impl Closure {
+    /// Create a [Closure] for a given store path. This will query
+    /// the local database for this information. No checks whether
+    /// the files actually exist are performed.
+    pub fn for_path(path: StorePathRef) -> Result<Self> {
+        let conn = Connection::open("/nix/var/nix/db/db.sqlite")?;
+
+        let path_infos = closure_path_infos(&conn, path)?;
+
+        let closure_paths: Vec<String> = path_infos
+            .iter()
+            .map(|x| x.path.to_absolute_path())
+            .collect();
+        let references = references_for_paths(&conn, &closure_paths)?;
+
+        Ok(Self {
+            path_infos,
+            references,
+        })
+    }
+
+    /// Get all the path infos of this [Closure].
+    pub fn all_path_infos(&self) -> &[LocalPathInfo] {
+        &self.path_infos
+    }
+
+    /// Get the references for a path that is part of the
+    /// [Closure]. Querying for a path outside of the closure
+    /// will yield an empty result.
+    pub fn references_for_path(
+        &self,
+        path_id: u64,
+    ) -> &[nix_compat::store_path::StorePath<String>] {
+        self.references
+            .get(&path_id)
+            .map(|val| val.as_slice())
+            .unwrap_or(&[])
+    }
+}
+
+/// An owning alternative to the [ExternalPathInfo] struct of Snix.
+///
+/// It does not contain the references, since we compute that separately.
+#[derive(Clone, Debug)]
+pub struct LocalPathInfo {
+    pub valid_path_id: u64,
+    pub nar_sha256: [u8; 32],
+    pub nar_size: u64,
+    pub path: nix_compat::store_path::StorePath<String>,
+    pub deriver: Option<nix_compat::store_path::StorePath<String>>,
+    pub signatures: Vec<nix_compat::narinfo::Signature<String>>,
+}
+
+/// Get a map of store paths to their references.
+fn references_for_paths(
+    connection: &Connection,
+    store_paths: &[String],
+) -> Result<HashMap<u64, Vec<nix_compat::store_path::StorePath<String>>>> {
+    let placeholders = std::iter::repeat_n("?", store_paths.len())
+        .collect::<Vec<_>>()
+        .join(",");
+    let references = format!("
+SELECT origin.id, target.path FROM ValidPaths origin JOIN Refs ON Refs.referrer = origin.id JOIN ValidPaths target ON Refs.reference = target.id WHERE origin.path IN ({})
+", placeholders);
+
+    let mut reference_query = connection.prepare(&references)?;
+    let mut rows = reference_query.query(rusqlite::params_from_iter(store_paths.iter()))?;
+
+    let mut reference_map = HashMap::<u64, Vec<nix_compat::store_path::StorePath<String>>>::new();
+
+    while let Some(row) = rows.next()? {
+        let id: u64 = row.get(0)?;
+        let path: String = row.get(1)?;
+        reference_map.entry(id).or_default().push(
+            nix_compat::store_path::StorePath::from_absolute_path_full(&path)
+                .expect("Could not create path from database entry")
+                .0,
+        )
+    }
+
+    Ok(reference_map)
+}
+
+/// Get the path infos of a closure of a store path.
+fn closure_path_infos(
+    connection: &Connection,
+    store_path: StorePathRef,
+) -> Result<Vec<LocalPathInfo>> {
     // Get the closure of a path
-    let mut statement = conn.prepare(
+    let mut statement = connection.prepare(
         "
 WITH RECURSIVE closure(
   id, path, hash, registrationTime, deriver, narSize, ultimate, sigs, ca
 ) AS (
-  -- seed row (bind :path or substitute literal)
+  -- Seed row
   SELECT
     id, path, hash, registrationTime, deriver, narSize, ultimate, sigs, ca
   FROM ValidPaths
-  WHERE path = '/nix/store/qg6nh6zf727nm87bkbn9jlkkslrm2wiw-lldb-20.1.6'
+  WHERE path = ?
 
   UNION
 
-  -- step: find rows that refer to any path already in the closure
+  -- Find rows that refer to any path already in the closure
   SELECT
     vp.id, vp.path, vp.hash, vp.registrationTime, vp.deriver, vp.narSize, vp.ultimate, vp.sigs, vp.ca
   FROM closure c
@@ -30,23 +131,39 @@ FROM closure
 ORDER BY id; "
     )?;
 
-    let closure_iter = statement.query_map([], |row| {
-        let path: String = row.get(1)?;
-        Ok(path)
-        // Ok(ExportedPathInfo {
-        //     closure_size: 0,
-        //     nar_sha256: row.get(2)?,
-        //     nar_size: row.get(4)?,
-        //     path: StorePathRef::from_absolute_path(path.as_bytes().as_ref())
-        //         .expect("Could not create path from database entry"),
-        //     deriver: None,
-        //     references: BTreeSet::new(),
-        //     signatures: vec![],
-        // })
-    })?;
+    statement
+        .query_map([store_path.to_absolute_path()], |row| {
+            let path_string: String = row.get(1)?;
+            let path = nix_compat::store_path::StorePath::from_absolute_path_full(&path_string)
+                .map(|x| x.0)
+                .expect("Could not create StorePath from the databasse");
+            let deriver_string: String = row.get(4)?;
+            let signatures_string: String = row.get(7)?;
+            let nar_sha256 =
+                NixHash::from_str(row.get_ref_unwrap(2).as_str()?, Some(HashAlgo::Sha256))
+                    .expect("Unable to construct a nar hash from the database")
+                    .digest_as_bytes()
+                    .try_into()
+                    .expect("Unable to convert a hash into 32 bytes of data");
 
-    for path in closure_iter {
-        println!("Found path{:?}", path?);
-    }
-    Ok(())
+            let deriver = (!deriver_string.is_empty()).then(|| {
+                nix_compat::store_path::StorePath::from_absolute_path_full(&deriver_string)
+                    .expect("Could not create path from database entry")
+                    .0
+            });
+            let signatures = signatures_string
+                .split_terminator(";")
+                .map(|s| Signature::parse(s).expect("Unable to parse signature"))
+                .collect();
+
+            Ok(LocalPathInfo {
+                valid_path_id: row.get(0)?,
+                nar_sha256,
+                nar_size: row.get(5)?,
+                path,
+                deriver,
+                signatures,
+            })
+        })?
+        .collect::<Result<Vec<_>, _>>()
 }
